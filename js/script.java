c5f5ec36861b6cda8ebf9e512e663914197bfb1c
Vini Veríssimo. <vidingo123@gmail.com>
2026-05-11 16:12:55 -0300

    Update comments and modify final photo styles
    Refactor comments and adjust grayscale filter for final photo.

--- js/script.java
+++ js/script.java
@@ -1,43 +1,48 @@
 const completed = JSON.parse(localStorage.getItem("completed")) || [];
 
-/* PINTAR CORAÇÕES */
+/* CORAÇÕES */
 
 completed.forEach(id => {
+
   const heart = document.getElementById("c" + id);
 
   if(heart){
+
     heart.innerHTML = "❤️";
+
     heart.classList.add("filled");
   }
+
 });
 
-/* EVOLUÇÃO DA FOTO FINAL */
+/* FOTO FINAL */
 
 const finalPhoto = document.getElementById("finalPhoto");
+
+/* EVOLUÇÃO */
 
 if(completed.length >= 1){
   finalPhoto.style.opacity = "0.7";
 }
 
 if(completed.length >= 2){
-  finalPhoto.style.filter = "grayscale(40%)";
+  finalPhoto.style.filter = "grayscale(30%)";
 }
 
 if(completed.length >= 3){
-  finalPhoto.classList.add("active");
-}
-
-/* DESBLOQUEAR FINAL */
-
-if(completed.length >= 3){
-
-  const finalPhoto = document.getElementById("finalPhoto");
 
   finalPhoto.classList.add("active");
+
+  finalPhoto.style.filter = "grayscale(0%)";
+
+  finalPhoto.style.opacity = "1";
 
   finalPhoto.style.cursor = "pointer";
 
   finalPhoto.addEventListener("click", () => {
+
     window.location.href = "final.html";
+
   });
+
 }
